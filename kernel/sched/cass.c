ef19ea32a6b7620b7096eef23ef092944ccad4dd
Yuan Si <do4suki@gmail.com>
2024-06-06 09:24:07 +0800

    fixup! sched/fair: Just use cass to select cpu for rt task
    Change-Id: I7096ee2e220216749f71a6e04986111a805b7a7c
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -319,11 +319,13 @@ static int cass_select_task_rq(struct task_struct *p, int prev_cpu,
 	return cass_best_cpu(p, prev_cpu, sync, rt);
 }
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
 				    int sd_flag, int wake_flags)
 {
 	return cass_select_task_rq(p, prev_cpu, wake_flags, false);
 }
+#endif
 
 int cass_select_task_rq_rt(struct task_struct *p, int prev_cpu,
  			   int sd_flag, int wake_flags)
