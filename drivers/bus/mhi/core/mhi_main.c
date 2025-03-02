d73cc1e3a3147360e2a78bb0dad2ff41114b568d
re-noroi <mochtur@gmail.com>
2025-03-02 16:11:34 +0700

    treewide: fix warning after removing debug
    Signed-off-by: re-noroi <mochtur@gmail.com>

--- drivers/bus/mhi/core/mhi_main.c
+++ drivers/bus/mhi/core/mhi_main.c
@@ -2206,7 +2206,6 @@ int mhi_debugfs_mhi_regdump_show(struct seq_file *m, void *d)
 int mhi_debugfs_mhi_states_show(struct seq_file *m, void *d)
 {
 	struct mhi_controller *mhi_cntrl = m->private;
-	struct mhi_link_info *cur_info = &mhi_cntrl->mhi_link_info;
 
 	return 0;
 }
