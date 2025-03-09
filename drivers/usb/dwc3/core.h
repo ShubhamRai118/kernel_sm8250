ed7a9192d4d6a634bea1fba452ecd5e7449df9a1
re-noroi <mochtur@gmail.com>
2025-03-10 05:55:36 +0700

    Revert "usb: dwc3: Queue wakeup worker from an IRQ worker"
    This reverts commit d3fb9d36ae935510ddb65debee2ca2c592e61797.

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -21,7 +21,6 @@
 #include <linux/debugfs.h>
 #include <linux/wait.h>
 #include <linux/workqueue.h>
-#include <linux/irq_work.h>
 
 #include <linux/usb/ch9.h>
 #include <linux/usb/gadget.h>
@@ -1287,7 +1286,6 @@ struct dwc3 {
 	const char		*hsphy_interface;
 
 	struct work_struct	wakeup_work;
-	struct irq_work		wakeup_irq_work;
 
 	unsigned		connected:1;
 	unsigned		delayed_status:1;
--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1734,18 +1734,11 @@ static int __dwc3_gadget_ep_queue(struct dwc3_ep *dep, struct dwc3_request *req)
 	return 0;
 }
 
-static void dwc3_gadget_wakeup_irq_work(struct irq_work *work)
-{
-	struct dwc3 *dwc = container_of(work, typeof(*dwc), wakeup_irq_work);
-
-	schedule_work(&dwc->wakeup_work);
-}
-
 static int dwc3_gadget_wakeup(struct usb_gadget *g)
 {
 	struct dwc3	*dwc = gadget_to_dwc(g);
 
-	irq_work_queue(&dwc->wakeup_irq_work);
+	schedule_work(&dwc->wakeup_work);
 	return 0;
 }
 
@@ -4337,7 +4330,6 @@ int dwc3_gadget_init(struct dwc3 *dwc)
 	dwc->irq_gadget = irq;
 
 	INIT_WORK(&dwc->wakeup_work, dwc3_gadget_wakeup_work);
-	init_irq_work(&dwc->wakeup_irq_work, dwc3_gadget_wakeup_irq_work);
 
 	dwc->ep0_trb = dma_alloc_coherent(dwc->sysdev,
 					  sizeof(*dwc->ep0_trb) * 2,
