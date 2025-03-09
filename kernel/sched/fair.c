danya2271/android_kernel_oneplus_sm8450

kernel-sched: dvfs_headroom: Rewrite for better power consumption in idle screen on and during watching videos

kernel/sched/fair.c
@@ -52,6 +52,10 @@ static unsigned int normalized_sysctl_sched_base_slice	= 5000000ULL;
  * parent will (try to) run first.
  */
 unsigned int sysctl_sched_child_runs_first __read_mostly;
+unsigned int sysctl_fps_threshold_high __read_mostly = 45;
+unsigned int sysctl_fps_threshold_low __read_mostly = 25;
+unsigned int sysctl_headroom_big __read_mostly = 18;
+unsigned int sysctl_util_low __read_mostly = 65;
 
 const_debug unsigned int sysctl_sched_migration_cost	= 0UL;
 
@@ -106,6 +110,46 @@ int __weak arch_asym_cpu_priority(int cpu)
  */
 unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
 #endif
+
+
+static struct ctl_table sched_fair_sysctls[] = {
+		{
+    		.procname       = "sched_fps_threshold_high",
+    		.data           = &sysctl_fps_threshold_high,
+    		.maxlen         = sizeof(unsigned int),
+    		.mode           = 0644,
+    		.proc_handler   = proc_dointvec,
+    	},
+		{
+    		.procname       = "sched_fps_threshold_low",
+    		.data           = &sysctl_fps_threshold_low,
+    		.maxlen         = sizeof(unsigned int),
+    		.mode           = 0644,
+    		.proc_handler   = proc_dointvec,
+    	},
+		{
+    		.procname       = "sched_headroom_big",
+    		.data           = &sysctl_headroom_big,
+    		.maxlen         = sizeof(unsigned int),
+    		.mode           = 0644,
+    		.proc_handler   = proc_dointvec,
+    	},
+		{
+    		.procname       = "sched_util_low",
+    		.data           = &sysctl_util_low,
+    		.maxlen         = sizeof(unsigned int),
+    		.mode           = 0644,
+    		.proc_handler   = proc_dointvec,
+    	},
+	{}
+};
+
+static int __init sched_fair_sysctl_init(void)
+{
+	register_sysctl_init("kernel", sched_fair_sysctls);
+	return 0;
+}
+late_initcall(sched_fair_sysctl_init);
 
 static inline void update_load_add(struct load_weight *lw, unsigned long inc)
 {

ShubhamRai118/kernel_sm8250

kernel/sched/fair.c
@@ -97,6 +97,11 @@ static unsigned int normalized_sysctl_sched_base_slice	= 3000000ULL;
 const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 
+unsigned int sysctl_fps_threshold_high __read_mostly = 45;
+unsigned int sysctl_fps_threshold_low __read_mostly = 25;
+unsigned int sysctl_headroom_big __read_mostly = 18;
+unsigned int sysctl_util_low __read_mostly = 65;
+
 int sched_thermal_decay_shift;
 static int __init setup_sched_thermal_decay_shift(char *str)
 {
@@ -163,6 +168,45 @@ unsigned int sysctl_walt_rtg_cfs_boost_prio = 99; /* disabled by default */
 unsigned int sysctl_walt_low_latency_task_threshold; /* disabled by default */
 __read_mostly unsigned int sysctl_sched_force_lb_enable = 1;
 #endif
+
+static struct ctl_table sched_fair_sysctls[] = {
+		{
+    		.procname       = "sched_fps_threshold_high",
+    		.data           = &sysctl_fps_threshold_high,
+    		.maxlen         = sizeof(unsigned int),
+    		.mode           = 0644,
+    		.proc_handler   = proc_dointvec,
+    	},
+		{
+    		.procname       = "sched_fps_threshold_low",
+    		.data           = &sysctl_fps_threshold_low,
+    		.maxlen         = sizeof(unsigned int),
+    		.mode           = 0644,
+    		.proc_handler   = proc_dointvec,
+    	},
+		{
+    		.procname       = "sched_headroom_big",
+    		.data           = &sysctl_headroom_big,
+    		.maxlen         = sizeof(unsigned int),
+    		.mode           = 0644,
+    		.proc_handler   = proc_dointvec,
+    	},
+		{
+    		.procname       = "sched_util_low",
+    		.data           = &sysctl_util_low,
+    		.maxlen         = sizeof(unsigned int),
+    		.mode           = 0644,
+    		.proc_handler   = proc_dointvec,
+    	},
+	{}
+};
+
+static int __init sched_fair_sysctl_init(void)
+{
+	register_sysctl("kernel", sched_fair_sysctls);
+	return 0;
+}
+late_initcall(sched_fair_sysctl_init);
 
 static inline void update_load_add(struct load_weight *lw, unsigned long inc)
 {